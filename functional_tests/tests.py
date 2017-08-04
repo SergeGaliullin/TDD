8ddec6fb8009f0cc638b6eb99daa49bd06180c0b
serge <sergei.galiullin@gmail.com>
2017-08-04 15:26:33 +0300

    Make functional tests an app, use LiveServerTestCase

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,10 +1,10 @@
 import time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-import unittest
+from django.test import LiveServerTestCase
 
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()        
@@ -20,7 +20,7 @@ class NewVisitorTest(unittest.TestCase):
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith has heard about a cool new online to-do app. She goes
         # to check out its homepage
-        self.browser.get('http://127.0.0.1:8000')
+        self.browser.get(self.live_server_url)
 
         # She notices the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
@@ -67,7 +67,3 @@ class NewVisitorTest(unittest.TestCase):
         # Satisfied, she goes back to sleep
 
         self.fail('Finish the test!')
-
-
-if __name__ == '__main__':
-    unittest.main(warnings='ignore')
--- lists/tests.py
+++ lists/tests.py
@@ -1,14 +1,8 @@
 from django.test import TestCase
-from django.urls import resolve
-from lists.views import home_page
 from lists.models import Item
 
 
 class HomePageTest(TestCase):
-
-    def test_root_url_resolves_to_home_page_view(self):
-        found = resolve('/')
-        self.assertEqual(found.func, home_page)
 
     def test_home_page_returns_correct_html(self):
         response = self.client.get('/')
