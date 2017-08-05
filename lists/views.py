4986d55ff58151090f8bc49ce1fc38d98edfbe56
serge <sergei.galiullin@gmail.com>
2017-08-05 17:03:17 +0300

    Refactor views and tests to deal with new URL

--- lists/views.py
+++ lists/views.py
@@ -3,13 +3,14 @@ from .models import Item
 
 
 def home_page(request):
-    if request.POST:
-        new_item_text = request.POST['item_text']
-        Item.objects.create(text=new_item_text)
-        return redirect('/lists/the-only-list-in-the-world')
     return render(request, 'home.html')
 
 
 def view_list(request):
     items = Item.objects.all()
     return render(request, 'lists.html', {'items': items})
+
+
+def new_list(request):
+    Item.objects.create(text=request.POST['item_text'])
+    return redirect('/lists/the-only-list-in-the-world/')
